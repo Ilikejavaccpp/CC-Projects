2f593af5cd4633d96926a4919d5f1da6feb7bc1f
Ilikejavaccpp <genomeacc2@gmail.com>
2025-11-01 15:32:36 +0000

    New

---
+++ C-gui/build/CMakeFiles/OpenGLApp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLApp.dir/include/glad.c.o"
+  "CMakeFiles/OpenGLApp.dir/include/glad.c.o.d"
+  "CMakeFiles/OpenGLApp.dir/src/main.c.o"
+  "CMakeFiles/OpenGLApp.dir/src/main.c.o.d"
+  "OpenGLApp"
+  "OpenGLApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OpenGLApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C-gui/src/main.c
@@ -0,0 +1,46 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <err.h>
+#include <stdlib.h>
+
+#ifndef nullptr
+#define nullptr NULL
+#endif
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello OpenGL", nullptr, nullptr);
+    if (!window) {
+        err(EXIT_FAILURE, "Failed to create GLFW window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        err(EXIT_FAILURE, "Failed to initialize GLAD\n");
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
